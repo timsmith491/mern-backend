f3c0f5eb352426c0695cc3ef25399586e04c6399
tsmith <tsmith@cartrawler.com>
2021-06-10 12:17:37 +0100

    User validation

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -67,6 +67,12 @@ const createPlace = (req, res, next) => {
 };
 
 const updatePlace = (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    throw new HttpError('Invalid inputs passed, check your data', 422);
+  }
+
   const { title, description } = req.body;
   const placeId = req.params.pid;
 
@@ -83,6 +89,9 @@ const updatePlace = (req, res, next) => {
 
 const deletePlace = (req, res, next) => {
   const placeId = req.params.pid;
+  if (!DUMMY_PLACES.find((p) => p.id === placeId)) {
+    throw new HttpError('Could not find a place for that id.', 404);
+  }
   DUMMY_PLACES = DUMMY_PLACES.filter((p) => p.id !== placeId);
   res.status(200).json({ message: 'Deleted place' });
 };
