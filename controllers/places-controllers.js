876dbf7f7a929798a76db9f714738a8d40f36214
tsmith <tsmith@cartrawler.com>
2021-06-10 14:43:50 +0100

    Geocoding API intergration

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -2,6 +2,7 @@ const uuid = require('uuid').v4;
 const { validationResult } = require('express-validator');
 
 const HttpError = require('../models/http-error');
+const getCoordsForAddress = require('../util/location');
 
 let DUMMY_PLACES = [
   {
@@ -44,19 +45,27 @@ const getPlacesByUserId = (req, res, next) => {
   res.json({ places });
 };
 
-const createPlace = (req, res, next) => {
+const createPlace = async (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    throw new HttpError('Invalid inputs passed, check your data', 422);
+    return next(new HttpError('Invalid inputs passed, check your data', 422));
   }
 
-  const { title, description, coordinates, address, creator } = req.body;
+  const { title, description, address, creator } = req.body;
+
+  let coordinates;
+  try {
+    coordinates = await getCoordsForAddress(address);
+  } catch (error) {
+    return next(error);
+  }
+
   const createdPlace = {
     id: uuid(),
     title,
     description,
-    coordinates: coordinates,
+    location: coordinates,
     address,
     creator,
   };
