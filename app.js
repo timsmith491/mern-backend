482cac5f8c412ad76340ccf043d28d698674a0c6
tsmith <timsmith491@gmail.com>
2021-06-10 16:25:51 +0100

    Add mongoose

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const placesRoutes = require('./routes/places-routes');
 const usersRoutes = require('./routes/users-routes');
